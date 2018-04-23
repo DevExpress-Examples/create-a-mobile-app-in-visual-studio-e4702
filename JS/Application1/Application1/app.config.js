7ab76df8b2c6b58c32d62c2f48c563a483931e67
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/Application1/Application1/app.config.js
@@ -0,0 +1,17 @@
+window.Application1 = $.extend(true, window.Application1, {
+  "config": {
+    "defaultLayout": "navbar",
+    "navigation":  [
+    {
+        title: "Home",
+        action: "#home",
+        icon: "home"
+    },
+    {
+        title: "About",
+        action: "#about",
+        icon: "info"
+    }
+    ]
+  }
+});
---
+++ JS/Application1/Application1/views/home.js
@@ -0,0 +1,23 @@
+﻿Application1.home = function (params) {
+    var viewModel = {
+        dataSource: DevExpress.data.createDataSource({
+            load: function (loadOptions) {
+                if (loadOptions.refresh) {
+                    var deferred = new $.Deferred();
+                    $.get('http://sampleservices.devexpress.com/api/Categories')
+                    .done(function (result) {
+                        var mapped = $.map(result, function (data) {
+                            return {
+                                name: data.CategoryName,
+                                id: data.CategoryID
+                            }
+                        });
+                        deferred.resolve(mapped);
+                    });
+                    return deferred;
+                }
+            }
+        })
+    };
+    return viewModel;
+};
