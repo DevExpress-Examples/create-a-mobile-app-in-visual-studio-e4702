15dc3c0b1b15eeac233f6be4a6ed13827c0a51dc
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/Application1.Desktop/index.js
@@ -0,0 +1,21 @@
+
+$(function() {
+    var startupView = "About";
+
+    DevExpress.devices.current("desktop");
+
+    Application1.app = new DevExpress.framework.html.HtmlApplication({
+        namespace: Application1,
+        layoutSet: DevExpress.framework.html.layoutSets[Application1.config.layoutSet],
+        mode: "webSite",
+        navigation: Application1.config.navigation,
+        commandMapping: Application1.config.commandMapping
+    });
+
+    $(window).unload(function() {
+        Application1.app.saveState();
+    });
+
+    Application1.app.router.register(":view/:id", { view: startupView, id: undefined });
+    Application1.app.navigate();
+});
